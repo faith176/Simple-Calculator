5c954de05627ae65bfd9119b0529fa39cceb73b5
faith176 <feyi.adesanya176@gmail.com>
2021-03-12 20:00:37 -0700

    radiobuttons work

--- app/src/main/java/com/example/simple_calculator/BaseConverter.java
+++ app/src/main/java/com/example/simple_calculator/BaseConverter.java
@@ -7,10 +7,13 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class BaseConverter extends AppCompatActivity {
 
@@ -27,6 +30,33 @@ public class BaseConverter extends AppCompatActivity {
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
         int colorCodeDark = Color.parseColor("#63939c");
         window.setStatusBarColor(colorCodeDark);
+
+    }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.decimal_base:
+                if (checked)
+                    currentBase="decimal";
+                    break;
+            case R.id.binary_base:
+                if (checked)
+                    currentBase="binary";
+                    break;
+            case R.id.hex_base:
+                if (checked)
+                    currentBase="hex";
+                break;
+            case R.id.octal_base:
+                if (checked)
+                    currentBase="octal";
+                break;
+        }
+        Log.d("check base", currentBase);
     }
 
     public void calculatorClick(View view) {
@@ -34,6 +64,5 @@ public class BaseConverter extends AppCompatActivity {
         intent1 = new Intent(BaseConverter.this, MainActivity.class);
         startActivity(intent1);
     }
-
 
 }
